076b7f2b345fec06deef09de8c4e46fc5038e9a4
Nick <holland_dominik@yahoo.de>
2024-01-07 19:35:45 +0100

    addition

--- Uni/Uebung10/Complexity.java
+++ Uni/Uebung10/Complexity.java
@@ -1,9 +1,5 @@
 package Uebung10;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.RoundingMode;
-
 public class Complexity {
 
 	private static String unitOfTime(double x) {
@@ -14,28 +10,9 @@ public class Complexity {
 		else return "s";
 	}
 
-	private static String unitOfTime(BigDecimal x) {
-		if (x.compareTo(BigDecimal.valueOf(0)) < 0) return "x must be greater than or equal to 0!";
-		else if (x.compareTo(BigDecimal.valueOf(1_000)) < 0) return "ns";
-		else if (x.compareTo(BigDecimal.valueOf(1_000_000)) < 0) return "μs";
-		else if (x.compareTo(BigDecimal.valueOf(1_000_000_000)) < 0) return "ms";
-		else if (x.compareTo(BigDecimal.valueOf(60_000_000_000L)) < 0) return "s";
-		else if (x.compareTo(BigDecimal.valueOf(3_600_000_000_000L)) < 0) return "min";
-		else return "h";
-	}
-
 	private static double timeAdjustment(double x) { while (x > 999) x /= 1000; return x; }
-	private static BigDecimal timeAdjustment(BigDecimal x) {
-		if (!unitOfTime(x).equals("s")) {
-			while (x.compareTo(BigDecimal.valueOf(999)) > 0) x = x.divide(BigDecimal.valueOf(1000));
-        } else {
-			while (x.compareTo(BigDecimal.valueOf(59)) > 0) x = x.divide(BigDecimal.valueOf(60));
-        }
-        return x;
-    }
 
 	private static String needsRounding(double x) { return x > 999 ? ".3" : ".0"; }
-	private static String needsRounding(BigDecimal x) { return x.compareTo(BigDecimal.valueOf(999)) > 0 ? ".3" : ".0"; }
 
 	public static void main(String[] args) {
 		System.out.println("Tabelle der Komplexitäten");
@@ -47,6 +24,7 @@ public class Complexity {
 
 		for (int k = 1; k < 10; k++) {
 			System.out.print(k);
+
 			System.out.printf("%" + (SPACES[0] - unitOfTime(k).length()) + "d" + unitOfTime(k), k); // ldn = k
 
 			int n = 1;
@@ -62,12 +40,13 @@ public class Complexity {
 			System.out.printf("%" + (SPACES[4] - unitOfTime(n * n * n).length()) + needsRounding(n * n * n) + "f" +
 					unitOfTime(n * n * n), timeAdjustment(n * n * n));
 
-			if (k > 5) {
-				BigDecimal m = BigDecimal.TWO;
-				for (int i = 1; i <= k; i++) m = m.multiply(m);
+			if (k <= 5) {
+				long m = 2;
+				for (int l = 1; l <= k; l++) m *= m;
 				System.out.printf("%" + (SPACES[5] - unitOfTime(m).length()) + needsRounding(m) + "f" +
 						unitOfTime(m), timeAdjustment(m));
 			}
+			if (k > 5) System.out.printf("%" + SPACES[5] + "s", "value too large");
 
 			System.out.println();
 		}
--- Uni/Uebung10/Complexity1.md
+++ Uni/Uebung10/Complexity1.md
@@ -1,1 +1,23 @@
-*** file for solution ***
+Insertion-Sort: O(n²)
+Selection-Sort: O(n²)
+Bubble-Sort: O(n²)
+Mergesort: O(nlogn)
+Quicksort: O(nlogn)
+binäres Suchen: O(logn)
+schnelles Potenzieren: O(logn)
+Berechnung der Quersumme einer Dezimalzahl: O(d) (d = Anzahl der Ziffern)
+Fibonacci-Zahlen: iterativ O(n), rekursiv O(2^n)
+
+(a) for (int i = 0; i < n; ++i); -> O(n)
+
+(b) for (int i = 100; i < n; ++i); -> O(n)
+
+(c) for (int i = 1; i <= n; i=i+i); -> O(logn)
+
+(d) for (int i = 1; i <= n; ++i)
+        for (int j = n; j > i; --j); -> O(n²)
+
+(e) for (int i = n/2; i < n; ++i); -> O(n)
+
+(f) for (int i = 0; i < n ; ++i)
+        for (int j = 1; j <= n*n; ++j); -> O(n²)
