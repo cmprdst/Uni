683090cfc3cda785fab083c8517569af60dead98
Nick <holland_dominik@yahoo.de>
2024-04-27 20:18:05 +0200

    addition

---
+++ Algorithmen und Datenstrukturen/Uebung3/MySList.java
@@ -0,0 +1,197 @@
+package Uebung3;
+
+import java.util.NoSuchElementException;
+
+/**MySList represents a single linked list, which holds elements of type
+   integer.*/
+public class MySList implements Iterable<Integer> {
+  //-----------------------------------------------------------------//
+  //------------- !!Do not change the following lines!! -------------//
+  public String toString() {
+    if (empty())
+      return "[]";
+    String rv = "[";
+    Node node = head_;
+    do {
+      rv += node.data_;
+      if (node.next_ != null)
+        rv += ",";
+      node = node.next_;
+    } while(node != null);
+    rv += "]";
+    return rv;
+  }
+  //-----------------------------------------------------------------//
+  //---------------- !!Insert your solution below!! -----------------//
+  /**Node represents a single element in the list.*/
+  class Node {
+    int data_;                         //!! Do not change this line !!
+    Node next_;
+
+    public Node(int data, Node next) {
+      data_ = data;
+      next_ = next;
+    }
+    public Node(int data) {
+      data_ = data;
+      next_ = null;
+    }
+  }
+  //----------------------------------------------------------------//
+  Node head_ = null;
+  //----------------------------------------------------------------//
+  public MySList() {}
+  //----------------------------------------------------------------//
+  public boolean empty() { return head_ == null; }
+  public int size() {
+    Node current = head_; int counter = 0;
+
+    while (current != null) { current = current.next_; counter++; }
+
+    return counter;
+  }
+
+  Node check(Node node) {
+    if (node == null) throw new NoSuchElementException("Checked node equals null.");
+    return node;
+  }
+
+  public int front() { return check(head_).data_; }
+  public int back() {
+    Node current = head_;
+
+    while (current.next_ != null) current = current.next_;
+
+    return current.data_;
+  }
+
+  public int atIndex(int i) {
+    if (i >= size()) { System.err.println("Cannot return a value as Node in question is out of bounds."); return -1; }
+
+    Node current = head_; int counter = 0;
+
+    while (counter < i) { current = current.next_; i++; }
+
+    return current.data_;
+  }
+  public int atPlace(int i) { return atIndex(--i); }
+
+  public void push_front(int i) { head_ = new Node(i, head_); }
+  public void push_back(int i) {
+    if (empty()) head_ = new Node(i);
+    else {
+      Node current = head_;
+
+      while (current.next_ != null) current = current.next_;
+
+      current.next_ = new Node(i);
+    }
+  }
+
+  public void insertValueAtIndex(int i, int value) {
+    if (i < 0) System.err.println("Cannot insert at a negative position.");
+    else if (i == 0) push_front(i);
+    else if (i >= size()) push_back(i);
+    else {
+      Node current = head_; int counter = 0;
+
+      while (counter < i - 1) { current = current.next_; counter++; }
+
+      Node nodeAfterValue = current.next_;
+      current.next_ = new Node(value, nodeAfterValue);
+    }
+  }
+  public void insertValueAtPlace (int i, int value) { insertValueAtIndex(--i, value); }
+  public void insertValueAfterNode(Node node, int value) {
+    if (empty()) head_ = new Node(value);
+    else if (node == null) head_ = new Node(value, head_);
+    else {
+      Node nodeAfterValue = node.next_;
+      node.next_ = new Node(value, nodeAfterValue);
+    }
+  }
+
+  public void pop_front() {
+    if (empty()) System.err.println("Cannot remove from an empty list.");
+    else if (size() == 1) head_ = null;
+    else head_ = head_.next_;
+  }
+  public void pop_back() {
+    if (empty() || size() == 1) pop_front();
+
+    Node current = head_;
+
+    while (current.next_.next_ != null) current = current.next_;
+
+    current.next_ = null;
+  }
+
+  public void removeValueAtIndex(int i) {
+    if (i < 0) System.err.println("Cannot remove from a negative position.");
+    else if (empty() || size() == 1 || i == 0) pop_front();
+    else if (i >= size() - 1) pop_back();
+    else {
+      Node current = head_; int counter = 0;
+
+      while (counter < i - 1) { current = current.next_; counter++; }
+
+      current.next_ = current.next_.next_;
+    }
+  }
+  public void removeValueAtPlace(int i) { removeValueAtIndex(--i); }
+
+  public void clear() { head_ = null; }
+  //----------------------------------------------------------------//
+  /**Iterator is used to traverse odd elements in a list.*/
+  public class Iterator implements java.util.Iterator<Integer> {
+    Node node_ = null;
+    Iterator(Node node) { node_ = node; }
+
+    @Override
+    public boolean hasNext() {
+      while (node_ != null) {
+        if (node_.data_ % 2 != 0) return true;
+        node_ = node_.next_;
+      }
+      return false;
+    }
+
+    @Override
+    public Integer next() {
+      if (!hasNext()) throw new NoSuchElementException("There is no next odd integer to iterate over.");
+
+      while (node_ != null) {
+        int currentData = node_.data_;
+        node_ = node_.next_;
+        if (currentData % 2 != 0) return currentData;
+      }
+      return null;
+    }
+
+    @Override
+    public void remove() { throw new UnsupportedOperationException(); }
+
+//    @Override
+//    public boolean equals(Object other) { return node_ == ((Iterator) other).node_; }
+
+    public void insertAtIteratorPos(Iterator i, int value) { insertValueAfterNode(i.node_, value); }
+  }
+  //----------------------------------------------------------------//
+  @Override
+  public Iterator iterator() { return new Iterator(head_); }
+  //----------------------------------------------------------------//
+  public static void main(String[] args) {
+    MySList example = new MySList();
+    example.push_back(85);
+    example.push_back(72);
+    example.push_back(93);
+    example.push_back(81);
+    example.push_back(74);
+    example.push_back(42);
+    System.out.println(example);
+
+    MySList test = new MySList();
+    for (int value : example) test.push_back(value);
+    System.out.println(test);
+  }
+}
